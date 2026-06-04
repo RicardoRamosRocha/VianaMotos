887fb3c5c314050fc0cca89edc608e159cff0369
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-06-04 18:21:57 -0300

    Criada Area Admin e cadastro inicial de marcas

---
+++ VianaMotos.Web/Areas/Admin/Controllers/MarcasController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VianaMotos.Web.Data;
+using VianaMotos.Web.Models;
+
+namespace VianaMotos.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class MarcasController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public MarcasController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var marcas = await _context.Marcas
+            .OrderBy(x => x.Nome)
+            .ToListAsync();
+
+        return View(marcas);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Marca marca)
+    {
+        if (!ModelState.IsValid)
+        {
+            var erros = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+
+            return Content(string.Join(" | ", erros));
+        }
+
+        _context.Marcas.Add(marca);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
