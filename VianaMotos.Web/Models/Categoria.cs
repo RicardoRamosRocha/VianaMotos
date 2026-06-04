f3afcfcea98de2e0583d69756d2b18c1111d98ab
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-06-03 22:52:36 -0300

    Criada entidade Categoria

---
+++ VianaMotos.Web/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VianaMotos.Web.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Nome { get; set; } = string.Empty;
+
+    public bool Ativa { get; set; } = true;
+}
