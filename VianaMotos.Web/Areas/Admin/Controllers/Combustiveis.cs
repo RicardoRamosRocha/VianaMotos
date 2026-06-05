554b36219c9a503eca188647878cbf0b198a8785
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-06-04 23:51:22 -0300

    CRUD completo de combustiveis

---
+++ VianaMotos.Web/Areas/Admin/Controllers/Combustiveis.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VianaMotos.Web.Data;
+using VianaMotos.Web.Models;
+
+namespace VianaMotos.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CombustiveisController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public CombustiveisController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var combustiveis = await _context.Combustiveis
+            .OrderBy(x => x.Nome)
+            .ToListAsync();
+
+        return View(combustiveis);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Combustivel combustivel)    
+    {
+        if (!ModelState.IsValid)
+        {
+            var erros = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+
+            return Content(string.Join(" | ", erros));
+        }
+
+        _context.Combustiveis.Add(combustivel);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var combustivel = await _context.Combustiveis.FindAsync(id);
+
+        if (combustivel == null)
+            return NotFound();
+
+        return View(combustivel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Combustivel combustivel)
+    {
+        if (id != combustivel.Id)
+            return NotFound();
+
+        if (!ModelState.IsValid)
+            return View(combustivel);
+
+        _context.Update(combustivel);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var combustivel = await _context.Combustiveis.FindAsync(id);
+
+        if (combustivel == null)
+            return NotFound();
+
+        return View(combustivel);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var combustivel = await _context.Combustiveis.FindAsync(id);
+
+        if (combustivel != null)
+        {
+            _context.Combustiveis.Remove(combustivel);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
--- VianaMotos.Web/obj/Debug/net9.0/VianaMotos.Web.AssemblyInfo.cs
+++ VianaMotos.Web/obj/Debug/net9.0/VianaMotos.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VianaMotos.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbd5264c9f0ef84afa1a08a3d06f3fa4975623c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fb948ff48c07b702255bf311e89fe7c791e1ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("VianaMotos.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VianaMotos.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
